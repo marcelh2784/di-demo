800d2296ee5190c6cae53bd3cbc4b362fa9b7793
MH <marcelh2784@gmail.com>
2018-03-25 15:39:15 +0200

    Chapter 29: The Spring Framework Context

---
+++ src/main/java/nl/marbink/didemo/controllers/MyController.java
@@ -0,0 +1,12 @@
+package nl.marbink.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+        System.out.println("Hello!");
+        return "foo";
+    }
+}
