01f1bdeae5adb0f522a3418aa318f90fd49a9253
MH <marcelh2784@gmail.com>
2018-03-28 16:44:22 +0200

    Chapter 70: Property Source configuration

--- src/main/java/nl/marbink/didemo/DiDemoApplication.java
+++ src/main/java/nl/marbink/didemo/DiDemoApplication.java
@@ -1,9 +1,11 @@
 package nl.marbink.didemo;
 
 import nl.marbink.didemo.controllers.MyController;
+import nl.marbink.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class DiDemoApplication {
@@ -12,5 +14,8 @@ public class DiDemoApplication {
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 		MyController controller = (MyController) ctx.getBean("myController");
 		controller.hello();
+
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+		System.out.println(fakeDataSource.getUser());
 	}
 }
